856810282c2c3a79d67540e9971dd784fb79f522
otaviodecastro <otaviognr@hotmail.com>
2017-07-30 08:32:23 -0300

    atividades

---
+++ E08/action.php
@@ -0,0 +1,19 @@
+<?php
+
+
+echo $_POST['username']; 
+echo ("<br />");
+echo $_POST['genero'];  
+echo ("<br />");
+echo $_POST['email'];  
+echo ("<br />"); 
+echo $_POST['cpf'];
+echo ("<br />");
+echo $_POST['tel'];
+echo ("<br />");
+echo $_POST['estado'];
+echo ("<br />");
+echo $_POST['cidade'];   
+echo ("<br />");      
+
+?>
